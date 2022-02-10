a4a699ed75b718fcfa3f62d2e9e3badd9c7fc0c2
psikac <patriksikac2@gmail.com>
2022-02-10 12:23:45 +0100

    dodan pokusaj responzivnog menija

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   collapsed = true;
+  resizeTimeout : any;
+  throttled : boolean = false;
+  delay : number = 250;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onCollapseToggled(){
+    this.collapsed = !this.collapsed;
+    console.log(this.collapsed);
+  }
+
+  onWindowResized(){
+    if(!this.throttled){
+      this.collapsed = true;
+      this.throttled = true;
+      console.log('collapse');
+      setTimeout(() => {
+        this.throttled = false;
+      }, this.delay);
+      
+    }
+    
+  }
 }
