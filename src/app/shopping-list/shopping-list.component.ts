fecc9780bfd6ee3727f85fd4995a4e8cd037026d
psikac <patriksikac2@gmail.com>
2022-02-19 18:00:28 +0100

    Dodana shopping lista, forma za dodavanje u listu Detaljiziran recipe-list.componente

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import { Ingridient } from '../shared/ingridient.model';
 
 @Component({
     selector: 'app-shopping-list',
@@ -7,6 +8,12 @@ import {Component, OnInit} from '@angular/core';
 })
 
 export class ShoppingListComponent implements OnInit{
+
+    ingridients: Ingridient[] = [
+        new Ingridient('Tomato', 1),
+        new Ingridient('Potato', 10)
+    ];
+
     constructor() {}
 
     ngOnInit(): void {
